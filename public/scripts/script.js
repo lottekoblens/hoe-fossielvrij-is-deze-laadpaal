44f2fee85e38e4c35875a52682515dee7f8df3f0
Lotte Koblens <lotte.koblens@hva.nl>
2022-05-25 13:56:48 +0200

    added some example markers

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -7,3 +7,41 @@ const map = new mapboxgl.Map({
     zoom: 9, // starting zoom
     center: [4.899431, 52.379189]
 });
+
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-77.032, 38.913]
+            },
+            properties: {
+                title: 'Mapbox',
+                description: 'Washington, D.C.'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [-122.414, 37.776]
+            },
+            properties: {
+                title: 'Mapbox',
+                description: 'San Francisco, California'
+            }
+        }
+    ]
+};
+
+// add markers to map
+for (const feature of geojson.features) {
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+    console.log('hpi')
+
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map);
+}
